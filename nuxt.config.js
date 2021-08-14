5e7cb4650d3451811ad9c411a0c1784f8d09a870
Devin Eldreth <devin.eldreth@gmail.com>
2021-08-13 23:08:55 -0400

    fix: metadata and favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,11 +4,15 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'nuxt-deldreth.me',
+    title: 'deldreth.me',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Articles and Apps by Devin Eldreth',
+      },
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
